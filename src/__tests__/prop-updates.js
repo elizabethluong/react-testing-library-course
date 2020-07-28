fe37af0667ad65d989c9a3dc561c57c5c77ad8d9
Elizabeth luong <elizabethluong@live.co.uk>
2020-07-28 13:43:38 +0100

    Add queryByRole to assert error message is not rendered

--- src/__tests__/prop-updates.js
+++ src/__tests__/prop-updates.js
@@ -7,12 +7,11 @@ import {FavoriteNumber} from '../favorite-number'
 test('entering an invalid value shows an error message', () => {
   // 🐨 you'll need to add queryByTestId, and rerender
   // 📖 learn how queryBy* queries differ from getBy* queries: https://github.com/kentcdodds/react-testing-library/blob/61e382f10d2d8d0be458103b7c267101541ed952/README.md#query-apis
-  const {getByLabelText, getByRole, rerender} = render(<FavoriteNumber />)
+  const {getByLabelText, getByRole, queryByRole, rerender} = render(<FavoriteNumber />)
   const input = getByLabelText(/favorite number/i)
   fireEvent.change(input, {target: {value: 10}})
-  expect(getByRole('error-message')).toHaveTextContent(
-    /the number is invalid/i,
-  )
+  expect(getByRole('error-message')).toHaveTextContent(/the number is invalid/i)
   // 🐨 call rerender with the FavoriteNumber and a max prop of 10.
   rerender(<FavoriteNumber max={10} />)
+  expect(queryByRole('error-messages')).toBeNull()
 })
