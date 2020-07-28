a023313d62a4709928423aa3dab54ceb239697df
Elizabeth luong <elizabethluong@live.co.uk>
2020-07-28 10:13:40 +0100

    add Jest-dom for better assertions

--- src/__tests__/react-dom.js
+++ src/__tests__/react-dom.js
@@ -4,6 +4,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 // 🐨 Here's your component:
 import {FavoriteNumber} from '../favorite-number'
+//import {toHaveAttribute, toHaveTextContent} from '@testing-library/jest-dom'
+//expect.extend({toHaveAttribute, toHaveTextContent})
+
+//Can remove the two lines of code above by importing the below:
+import '@testing-library/jest-dom/extend-expect'
 
 test('renders a number input with a label "Favorite Number"', () => {
   // 🐨 create a div (💯 document.createElement)
@@ -11,7 +16,7 @@ test('renders a number input with a label "Favorite Number"', () => {
   // 🐨 render the FavoriteNumber component to that div with ReactDOM.render
   ReactDOM.render(<FavoriteNumber />, div)
   // 🐨 assert the input type attribute is a number
-  expect(div.querySelector('input').type).toBe('number')
+  expect(div.querySelector('input')).toHaveAttribute('type', 'number')
   //:🐨 assert the label's text content is "Favorite Number"
-  expect(div.querySelector('label').textContent).toBe('Favorite Number')
+  expect(div.querySelector('label')).toHaveTextContent('Favorite Number')
 })
