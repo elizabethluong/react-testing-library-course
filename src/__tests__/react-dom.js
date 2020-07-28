760ff9efdf513a89192b1ae199f32ace95facf02
Elizabeth luong <elizabethluong@live.co.uk>
2020-07-28 11:22:27 +0100

    Use DOM testing library to make sure label and input are associated and remove any false positives

--- src/__tests__/react-dom.js
+++ src/__tests__/react-dom.js
@@ -1,13 +1,16 @@
+import {getQueriesForElement} from '@testing-library/react'
 // 🐨 you're going to need to use React to create react elements, so import react
 import React from 'react'
 // 🐨 we're going to render the FavoriteNumber component with ReactDOM so you'll need to import react-dom
 import ReactDOM from 'react-dom'
 // 🐨 Here's your component:
 import {FavoriteNumber} from '../favorite-number'
+
+// 🐨 For better assertions, use:
 //import {toHaveAttribute, toHaveTextContent} from '@testing-library/jest-dom'
 //expect.extend({toHaveAttribute, toHaveTextContent})
 
-//Can remove the two lines of code above by importing the below:
+// 🐨 Can remove the two lines of code above by importing the below:
 import '@testing-library/jest-dom/extend-expect'
 
 test('renders a number input with a label "Favorite Number"', () => {
@@ -16,7 +19,7 @@ test('renders a number input with a label "Favorite Number"', () => {
   // 🐨 render the FavoriteNumber component to that div with ReactDOM.render
   ReactDOM.render(<FavoriteNumber />, div)
   // 🐨 assert the input type attribute is a number
-  expect(div.querySelector('input')).toHaveAttribute('type', 'number')
-  //:🐨 assert the label's text content is "Favorite Number"
-  expect(div.querySelector('label')).toHaveTextContent('Favorite Number')
+  const {getByLabelText} = getQueriesForElement(div)
+  const input = getByLabelText(/favorite Number/i) //replace string with regex to ignore casing as end user doesn't care
+  expect(input).toHaveAttribute('type', 'number')
 })
